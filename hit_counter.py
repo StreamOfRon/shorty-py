1570ff89f2de94167c822f302d7f8bee7ca37e2d
Ron Miller <ron@cloudswarm.net>
2022-12-22 09:44:31 -0800

    Fixing timestamp bug and adding docs to HitCounter

--- hit_counter.py
+++ hit_counter.py
@@ -5,6 +5,11 @@ import arrow
 
 
 class HitCounter(object):
+  """
+  Buffer the hit count and last-accessed data for short URLs so that we're not 
+  writing to the database with every read.  The intent is that flush_to_store 
+  will be called every N seconds by a background task.
+  """
   def __init__(self):
     self._counts = defaultdict(0)
     self._timestamp = dict()
@@ -13,7 +18,7 @@ class HitCounter(object):
   def add_hit(self, name):
     with self._lock:
       self._counts[name] += 1
-      self._timestamp = arrow.utcnow()
+      self._timestamp[name] = arrow.utcnow()
 
   def flush_to_store(self, datastore):
     pass
